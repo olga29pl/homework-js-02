fa089316ac3bc3db0d116c7eca2a6db05c56a9ee
Olga Plekan <pl.olga29@gmail.com>
2017-08-24 14:26:41 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const n = parseFloat(prompt('Enter the number'));
+ 
+function fib (n){
+    let x1 = 1, x2=1;
+    let x3;
+    for(let i=2;i<=n;i++){
+        x3=x1+x2;
+        x1=x2;
+        x3=x2;
+    }
+    return x3;
+}
+
+// function fib (n) {
+//     const sqrt = Math.sqrt(5);
+//     const a =(1+sqrt)/2;
+//     const b =(1-sqrt)/2;
+//     return (Math.pow(a,n)-Math.pow(b,n)/sqrt);
+// }
+
+// function fib(n){
+//     let number;
+//     if(n>2){
+//         number=fib(n-1)+fib(n-2);
+//     }else{
+//         number=1;
+//     }
+//     return number;
+//     }
+
+// function fib (n){
+//     let fibArr=[1,1];
+//     for (let i=2; i<n; i++){
+//         fibArr[i] = fibArr[i-1]+fibArr[i-2];
+//     }
+//     return fibArr[n-1];
+// }
+const fibonacci = fib(n);
+document.write(fibonacci);
+
+
+
